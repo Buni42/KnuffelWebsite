edfa4f9d5c883133f29a1f1f763733c098a25a4f
Buni42 <bunyaminanil88@gmail.com>
2025-02-19 16:20:50 +0100

    Added project to repo

---
+++ flasktest.py
@@ -0,0 +1,11 @@
+import flask 
+
+app = flask.Flask(__name__)
+# a simple hello world route
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
