4f2eeb2612554b257852952ad66f39a7023e35f0
Buni42 <bunyaminanil88@gmail.com>
2025-02-26 14:06:27 +0100

    hey

--- application.py
+++ application.py
@@ -18,5 +18,6 @@ def submit():
 if __name__ == '__main__':
     
     print("Starting Flask application...")
+    print("hey")
     print("Application is running on http://127.0.0.1:5000/hello")
     app.run(debug=True, port = 5000)
